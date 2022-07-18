7e53d3faa1f19fb6e855725bc688cc9cdd901389
Andres Zapata <me@azapata.io>
2022-07-18 21:13:33 +0200

    fix: :bug: Fix z-index with queueMicrotask box

--- src/queMicrotaskMoveForward.ts
+++ src/queMicrotaskMoveForward.ts
@@ -7,7 +7,9 @@ import { moveForward } from "./moveForward";
 const elementID = 'QMT-box'
 const element = document.getElementById(elementID) as HTMLDivElement;
 element.style.cursor = 'pointer'
+element.style.zIndex = '100'
 
 element.addEventListener('click', () => {
+  element.style.zIndex = '';
   moveForward(document.getElementById(elementID) as HTMLDivElement, window.queueMicrotask);
 })
